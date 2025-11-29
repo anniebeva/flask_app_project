a688a73cdbacb910a493ff6fc6a19aa08c9e06bd
anniebeva <anniebeva@gmail.com>
2025-11-29 13:00:32 +0300

    Assignment: project structure

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
-    return 'This is the homepage!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/hello')
 def hello():
